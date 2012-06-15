beef6a3d63e25fcb5b6c3e3344876b02523bf99c
Matthias Kestenholz <mk@spinlock.ch>
2012-06-15 09:33:48 +0200

    Stop using d.c.urls.defaults

--- yesimeanit/showoff/newsletter_subscriptions/urls.py
+++ yesimeanit/showoff/newsletter_subscriptions/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 from yesimeanit.views import (ConfirmationView, UnsubscriptionView)
 from .views import (SubscribeView, UnsubscribeView)
