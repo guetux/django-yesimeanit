201afb100214bfa2f31d19b5146189f0676d561e
Stefan Reinhard <sr@feinheit.ch>
2012-07-18 14:49:42 +0200

    add minimal setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='yesimeanit',
+    version='0.1',
+    author='Matthias Kestenholz',
+    author_email='mk@feinheit.ch',
+    url='http://github.com/matthiask/django-yesimeanit/',
+    license='BSD License',
+    packages=['yesimeanit'],
+)
